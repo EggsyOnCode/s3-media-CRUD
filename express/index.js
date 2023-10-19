08ba580d51e851d0fc477ea684af89bd4fb9e374
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-19 17:51:43 +0500

    added multer to recevie img files ; using meemoryStorage

--- express/index.js
+++ express/index.js
@@ -1,18 +1,29 @@
 import express from "express";
 import multer from "multer";
-
+import cors from "cors";
+import bodyParser from "body-parser";
 import { PrismaClient } from "@prisma/client";
 
+///multer config -- storing images temp in memorySTorage for processing etc
+
 const app = express();
+app
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(cors())
+  .use(bodyParser.json());
 const prisma = new PrismaClient();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.get("/api/posts", async (req, res) => {
   const posts = await prisma.posts.findMany({ orderBy: [{ created: "desc" }] });
+  console.log(req.body);
   res.send(posts);
 });
 
-app.post("/api/posts", async (req, res) => {
-  res.status(201).send(post);
+app.post("/api/posts", upload.single("image"), async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
 });
 
 app.delete("/api/posts/:id", async (req, res) => {
--- react/src/components/pages/NewPost.jsx
+++ react/src/components/pages/NewPost.jsx
@@ -1,40 +1,52 @@
-import { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
+export default function NewPost() {
+  const [file, setFile] = useState();
+  const [caption, setCaption] = useState("");
 
-export default function NewPost() {  
+  const navigate = useNavigate();
 
-  const [file, setFile] = useState()
-  const [caption, setCaption] = useState("")
+  const submit = async (event) => {
+    event.preventDefault();
 
-  const navigate = useNavigate()
-
-  const submit = async event => {
-    event.preventDefault()
+    const postUrl = "http://localhost:8080/api/posts";
 
     const formData = new FormData();
-    formData.append("image", file)
-    formData.append("caption", caption)
-    await axios.post("/api/posts", formData, { headers: {'Content-Type': 'multipart/form-data'}})
+    formData.append("image", file);
+    formData.append("caption", caption);
 
-    navigate("/")
-  }
+    await axios.post(postUrl, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
 
-  const fileSelected = event => {
-    const file = event.target.files[0]
-		setFile(file)
-	}
+    navigate("/");
+  };
+
+  const fileSelected = (event) => {
+    const file = event.target.files[0];
+    setFile(file);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center">
-
-        <form onSubmit={submit} style={{width:650}} className="flex flex-col space-y-5 px-5 py-14">
-          <input onChange={fileSelected} type="file" accept="image/*"></input>
-          <input value={caption} onChange={e => setCaption(e.target.value)} type="text" placeholder='Caption'></input>
-          <button type="submit">Submit</button>
-        </form>
-
+      <form
+        onSubmit={submit}
+        style={{ width: 650 }}
+        className="flex flex-col space-y-5 px-5 py-14"
+      >
+        <input onChange={fileSelected} type="file" accept="image/*"></input>
+        <input
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+          type="text"
+          placeholder="Caption"
+        ></input>
+        <button type="submit" onClick={submit}>
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
